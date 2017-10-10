5d63f14e5ad6b4e0b953aec64e00b1ba5a81542d
robot0321 <jychung0321@gmail.com>
2017-10-11 02:15:34 +0900

    almost HW2

--- Assignment2/ConsoleApplication1/ConsoleApplication1/selection.cpp
+++ Assignment2/ConsoleApplication1/ConsoleApplication1/selection.cpp
@@ -2,6 +2,7 @@
 #include "sort.h"
 #include <iostream>
 #include <Windows.h>
+#define group_ele_num 7
 
 int array_selection(int* array, int l, int r, int k){
 // Implement here!
@@ -81,67 +82,88 @@ int SELECT(int* array, int l, int r, int k){
 	if (l == r) {
 		return l;
 	}
-	debug(array, l, r);
 	int pivot_idx = partition(array, l, r);
-	int pivot_pos = pivot_idx - l + 1;
+	int pivot_pos = pivot_idx - l;
 	if (k == pivot_pos) {
 		return pivot_idx;
 	}
 	else if (k < pivot_pos) {
-		return quick_selection(array, l, pivot_idx - 1, k);
+		return SELECT(array, l, pivot_idx - 1, k);
 	}
 	else {
-		return quick_selection(array, pivot_idx + 1, r, k - pivot_pos);
+		return SELECT(array, pivot_idx + 1, r, k - pivot_pos);
 	}
 }
 
 int linear_selection(int* array, int l, int r, int k){
 // Implement here!
+	//std::cout << "d" << std::endl;
     if (l == r) {
 		return array[l];
 	}
-    debug(array, l, r);
+	//debug(array, l, r);
 
 	int length = r - l + 1;
-	int group_num = length / 7 + 1;
+	int group_num = length / group_ele_num + 1;
+	int last_group_num = length % group_ele_num;
 	int* median_arr = new int[group_num];
-	for (int i = 0; i < group_num; i++) {
-		insertion_sort(array, 7 * i, 7 * i + 6);
-		median_arr[i] = array[7 * i + 3];
+
+	for (int i = 0; i < group_num - 1; i++) {
+		insertion_sort(array, group_ele_num * i, group_ele_num * i + 6);
+		median_arr[i] = array[group_ele_num * i + 3];
+		//debug(array, l, r);
 	}
-	int good_pivot_idx = SELECT(median_arr, 0, group_num-1, group_num/2);
+
+	//std::cout << group_num << last_group_num << std::endl;
+	insertion_sort(array, group_ele_num * (group_num -1), group_ele_num * (group_num-1) + last_group_num -1);
+	//debug(array, l, r);
+	median_arr[group_num-1] = array[group_ele_num * (group_num-1) + last_group_num / 2];
+	//debug(median_arr, 0, group_num-1);
+
+	int good_pivot_idx = SELECT(median_arr, 0, group_num-1, (group_num-1)/2);
+
+	
 
 	delete[] median_arr;
+
 	int pivot_pos = good_pivot_idx - l + 1;
 	if (k == pivot_pos) {
 		return array[good_pivot_idx];
 	}
 	else if (k < pivot_pos) {
-		return linear_selection(array, l, good_pivot_idx - 1, k);
+		swap(array, good_pivot_idx, r);
+		int aa = partition(array, l, r);
+		return linear_selection(array, l, aa - 1, k);
 	}
 	else {
-		return linear_selection(array, good_pivot_idx + 1, r, k - pivot_pos);
+		swap(array, good_pivot_idx, r);
+		int aa = partition(array, l, r);
+		return linear_selection(array, aa + 1, r, k - pivot_pos);
 	}
-
-	return 0;
 }
 
 typedef int (*FP)(int*, int, int, int);
 
 int main() {
 	int arr[] = { 3, 2, 1, 6, 13, 5, 12, 15, 11, 10, 4, 9 };
+	int buff[] = { 3, 2, 1, 6, 13, 5, 12, 15, 11, 10, 4, 9 };
+
 	int k_th_num;
 	int size = 12;
 	int k = 8;
+	
 
 	const FP func_ptr[4] = { &array_selection,
 							 &heap_selection,
 							 &quick_selection,
 							 &linear_selection};
-
+	  
 	for (int i = 0; i < 4; i++) {
 		k_th_num = func_ptr[i](arr, 0, size - 1, k);
 		std::cout << k_th_num << std::endl;
+		for (int j = 0; j < size; j++) {
+			arr[j] = buff[j];
+		}
 	}
 
 	int a;
